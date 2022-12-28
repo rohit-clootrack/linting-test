e20c7f03bfb46e8c607b844908436e86e8394341
Rohit Agarwal <rohit.agarwal@clootrack.com>
2022-12-28 15:06:15 +0530

    Add me endpoint

--- linting_test/users/urls.py
+++ linting_test/users/urls.py
@@ -4,11 +4,13 @@ from linting_test.users.views import (
     user_detail_view,
     user_redirect_view,
     user_update_view,
+    user_get,
 )
 
 app_name = "users"
 urlpatterns = [
     path("~redirect/", view=user_redirect_view, name="redirect"),
     path("~update/", view=user_update_view, name="update"),
+    path("me/", view=user_get, name="me"),
     path("<str:username>/", view=user_detail_view, name="detail"),
 ]
