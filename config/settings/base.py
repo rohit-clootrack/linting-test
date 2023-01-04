355617961585f305c9cb43682fa3c0a6e785ffec
Abhishek <abhishek.sambhare@clootrack.com>
2023-01-04 15:55:05 +0530

    Test 1 - CRD

--- config/settings/base.py
+++ config/settings/base.py
@@ -79,6 +79,7 @@ THIRD_PARTY_APPS = [
 LOCAL_APPS = [
     "linting_test.users",
     # Your stuff: custom apps go here
+    "testapp",
 ]
 # https://docs.djangoproject.com/en/dev/ref/settings/#installed-apps
 INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
